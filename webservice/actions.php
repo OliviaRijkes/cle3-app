390506fe3961e610c3c67b45eae4b573d45acf29
Tuna <tkelemci@student.lentiz.nl>
2026-03-30 14:58:13 +0200

    Toevoeging php array aan actions.php

--- webservice/actions.php
+++ webservice/actions.php
@@ -1,260 +1,201 @@
 <?php
 
 
-/** * @return array */
-function getPlayers(): array
-{
-    return [
-        [
-            "id" => 1,
-            "name" => "Ugurcan Cakir",
-            "position" => "Goalkeeper",
-        ],
-        [
-            "id" => 2,
-            "name" => "Gunay Guvenc",
-            "position" => "Goalkeeper",
-        ],
-        [
-            "id" => 3,
-            "name" => "Sacha Boey",
-            "position" => "Right-Back",
-        ],
-        [
-            "id" => 4,
-            "name" => "Roland Sallai",
-            "position" => "Right Winger",
-        ],
-        [
-            "id" => 5,
-            "name" => "Wilfred Singo",
-            "position" => "Right-Back",
-        ],
-        [
-            "id" => 6,
-            "name" => "Davidson Sanchez",
-            "position" => "Centre-Back",
-        ],
-        [
-            "id" => 7,
-            "name" => "Abdulkerim Bardakci",
-            "position" => "Centre-Back",
-        ],
-        [
-            "id" => 8,
-            "name" => "Eren Elmali",
-            "position" => "Left-Back",
-        ],
-        [
-            "id" => 9,
-            "name" => "Ismail Jakobs",
-            "position" => "Left-Back",
-        ],
-        [
-            "id" => 10,
-            "name" => "Lucas Torreira",
-            "position" => "Defensive Midfielder",
-        ],
-        [
-            "id" => 11,
-            "name" => "Mario Lemina",
-            "position" => "Defensive Midfielder",
-        ],
-        [
-            "id" => 12,
-            "name" => "Ilkay Gundogan",
-            "position" => "Central Midfielder",
-        ],
-        [
-            "id" => 13,
-            "name" => "Yunus Akgun",
-            "position" => "Right Winger",
-        ],
-        [
-            "id" => 14,
-            "name" => "Baris Alper Yilmaz",
-            "position" => "Left Winger",
-        ],
-        [
-            "id" => 15,
-            "name" => "Noa Lang",
-            "position" => "Left Winger",
-        ],
-        [
-            "id" => 16,
-            "name" => "Leroy Sane",
-            "position" => "Right Winger",
-        ],
-        [
-            "id" => 17,
-            "name" => "Victor Osimhen",
-            "position" => "Striker",
-        ],
-        [
-            "id" => 18,
-            "name" => "Mauro Icardi",
-            "position" => "Striker",
-        ],
-        [
-            "id" => 19,
-            "name" => "Gabriel Sara",
-            "position" => "Attacking Midfielder",
-        ],
-        [
-            "id" => 19,
-            "name" => "Okan Buruk",
-            "position" => "Coach",
-        ]
-    ];
-}
+const products = [
 
-/**
- * @param $id
- * @return array|false
- */
-function getPlayerDetails($id): array|false
-{
-    $tags = [
-        1 => [
-            "nationality" => "Turkish",
-            "appearances" => 31,
-            "goals" => 0,
-            "assists" => 0,
-            "contract_until" => "30/06/2030"
-        ],
-        2 => [
-            "nationality" => "Turkish",
-            "appearances" => 13,
-            "goals" => 0,
-            "assists" => 0,
-            "contract_until" => "30/06/2028"
-        ],
-        3 => [
-            "nationality" => "France",
-            "appearances" => 10,
-            "goals" => 2,
-            "assists" => 3,
-            "contract_until" => "30/06/2026 (loan)"
-        ],
-        4 => [
-            "nationality" => "Hungary",
-            "appearances" => 37,
-            "goals" => 1,
-            "assists" => 6,
-            "contract_until" => "30/06/2028"
-        ],
-        5 => [
-            "nationality" => "Ivory Coast",
-            "appearances" => 21,
-            "goals" => 1,
-            "assists" => 2,
-            "contract_until" => "30/06/2030"
-        ],
-        6 => [
-            "nationality" => "Colombia",
-            "appearances" => 38,
-            "goals" => 2,
-            "assists" => 0,
-            "contract_until" => "30/06/2029"
-        ],
-        7 => [
-            "nationality" => "Turkish",
-            "appearances" => 38,
-            "goals" => 13,
-            "assists" => 1,
-            "contract_until" => "30/06/2027"
-        ],
-        8 => [
-            "nationality" => "Turkish",
-            "appearances" => 33,
-            "goals" => 4,
-            "assists" => 0,
-            "contract_until" => "30/06/2028"
-        ],
-        9 => [
-            "nationality" => "Senegal",
-            "appearances" => 30,
-            "goals" => 1,
-            "assists" => 2,
-            "contract_until" => "30/06/2028"
-        ],
-        10 => [
-            "nationality" => "Uruguay",
-            "appearances" => 38,
-            "goals" => 3,
-            "assists" => 4,
-            "contract_until" => "30/06/2027"
-        ],
-        11 => [
-            "nationality" => "Gabon",
-            "appearances" => 32,
-            "goals" => 1,
-            "assists" => 1,
-            "contract_until" => "30/06/2029"
-        ],
-        12 => [
-            "nationality" => "Germany",
-            "appearances" => 30,
-            "goals" => 2,
-            "assists" => 4,
-            "contract_until" => "30/06/2029"
-        ],
-        13 => [
-            "nationality" => "Turkish",
-            "appearances" => 34,
-            "goals" => 8,
-            "assists" => 8,
-            "contract_until" => "30/06/2026"
-        ],
-        14 => [
-            "nationality" => "Turkish",
-            "appearances" => 40,
-            "goals" => 10,
-            "assists" => 12,
-            "contract_until" => "30/06/2028"
-        ],
-        15 => [
-            "nationality" => "Netherlands",
-            "appearances" => 10,
-            "goals" => 2,
-            "assists" => 2,
-            "contract_until" => "30/06/2026 (loan)"
-        ],
-        16 => [
-            "nationality" => "Germany",
-            "appearances" => 30,
-            "goals" => 6,
-            "assists" => 8,
-            "contract_until" => "30/06/2028"
-        ],
-        17 => [
-            "nationality" => "Nigeria",
-            "appearances" => 28,
-            "goals" => 19,
-            "assists" => 7,
-            "contract_until" => "30/06/2029"
-        ],
-        18 => [
-            "nationality" => "Argentina",
-            "appearances" => 38,
-            "goals" => 15,
-            "assists" => 2,
-            "contract_until" => "30/06/2026"
-        ],
-        19 => [
-            "nationality" => "Brazil",
-            "appearances" => 38,
-            "goals" => 6,
-            "assists" => 3,
-            "contract_until" => "30/06/2029"
-        ],
-        20 => [
-            "nationality" => "Turkish",
-            "appearances" => 48,
-        ]
-    ];
-    return $tags[$id] ?? false;
-}
+    [
+        "id" => 1,
+        "name" => "AH Penne Rigate",
+        "category" => "Pasta & Rijst",
+        "price" => "1.25",
+        "stock" => 12,
+        "delivery" => "18-3-2026",
+        "ingredients" => "• Energie=> 268 kcal<br>• Vet=> 8.5g <br>• Verzadigd Vet=> 4.9g  <br>• Eiwitten=> 56g",
+        "image" => "🍝"
+    ],
+    [
+        "id" => 2,
+        "name" => "Lassie Toverrijst",
+        "category" => "Pasta & Rijst",
+        "price" => "2.10",
+        "stock" => 8,
+        "delivery" => "17-3-2026",
+        "ingredients" => "• Energie=> 152 kcal<br>• Vet=> 10.5g <br>• Verzadigd Vet=> 6.9g  <br>• Eiwitten=> 42g",
+        "image" => "🍚"
+    ],
+
+    // Snacks
+    [
+        "id" => 3,
+        "name" => "Lay's Naturel Chips",
+        "category" => "Snacks",
+        "price" => "1.89",
+        "stock" => 20,
+        "delivery" => "17-3-2026",
+        "ingredients" => "• Energie=> 212 kcal<br>• Vet=> 10.5g <br>• Verzadigd Vet=> 6.9g  <br>• Eiwitten=> 22g <br> • Aardappelen<br>• Plantaardige oliën<br>• Zout",
+        "image" => "🥔"
+    ],
+    [
+        "id" => 4,
+        "name" => "AH Borrelnootjes",
+        "category" => "Snacks",
+        "price" => "1.45",
+        "stock" => 15,
+        "delivery" => "19-3-2026",
+        "ingredients" => "• Energie=> 212 kcal<br>• Vet=> 10.5g <br>• Verzadigd Vet=> 6.9g  <br>• Eiwitten=> 22g <br> • Pinda's<br>• Zetmeel<br>• Kruidenmix",
+        "image" => "🥜"
+    ],
+
+    // Drinken
+    [
+        "id" => 5,
+        "name" => "Coca-Cola Original",
+        "category" => "Drinken",
+        "price" => "2.49",
+        "stock" => 50,
+        "delivery" => "17-3-2026",
+        "ingredients" => "•  Vet=> 3.9g  <br>• Suiker=> 80g <br>• Cafeïne",
+        "image" => "🥤"
+    ],
+    [
+        "id" => 6,
+        "name" => "Appelsap 1L",
+        "category" => "Drinken",
+        "price" => "1.15",
+        "stock" => 30,
+        "delivery" => "17-3-2026",
+        "ingredients" => "• 100% Appelsap uit concentraat <br>•  Vet=> 2.9g  <br>• Suiker=> 60g",
+        "image" => "🧃"
+    ],
+
+    // Vis & Kip
+    [
+        "id" => 7,
+        "name" => "AH Kipfilet 400g",
+        "category" => "Vis & Kip",
+        "price" => "5.50",
+        "stock" => 6,
+        "delivery" => "17-3-2026",
+        "ingredients" => "• Energie=> 212 kcal<br>• Vet=> 10.5g <br>• Verzadigd Vet=> 6.9g  <br>• Eiwitten=> 22g",
+        "image" => "🍗"
+    ],
+    [
+        "id" => 8,
+        "name" => "Diepvries Zalm",
+        "category" => "Vis & Kip",
+        "price" => "7.95",
+        "stock" => 4,
+        "delivery" => "18-3-2026",
+        "ingredients" => "• Energie=> 100 kcal<br>• Vet=> 1.5g <br>• Verzadigd Vet=> 1g  <br>• Eiwitten=> 30.5g",
+        "image" => "🐟"
+    ],
+
+    // Fruit & Groente
+    [
+        "id" => 9,
+        "name" => "Bananen (tros)",
+        "category" => "Fruit & Groente",
+        "price" => "1.69",
+        "stock" => 25,
+        "delivery" => "17-3-2026",
+        "ingredients" => "• Energie=> 47 kcal<br>• Vet=> 1.5g <br>• Eiwitten=> 30.5g",
+        "image" => "🍌"
+    ],
+    [
+        "id" => 10,
+        "name" => "Komkommer",
+        "category" => "Fruit & Groente",
+        "price" => "0.79",
+        "stock" => 40,
+        "delivery" => "17-3-2026",
+        "ingredients" => "• Energie=> 47 kcal<br>• Vet=> 1.5g <br>• Eiwitten=> 30.5g",
+        "image" => "🥒"
+    ],
+
+    [
+        "id" => 11,
+        "name" => "AH Paracetamol 500mg",
+        "category" => "Medicijnen",
+        "price" => "1.59",
+        "stock" => 10,
+        "delivery" => "17-3-2026",
+        "ingredients" => "• Nicotine=> 200mg",
+        "image" => "💊"
+    ],
+    [
+        "id" => 12,
+        "name" => "Ibuprofen 400mg",
+        "category" => "Medicijnen",
+        "price" => "2.25",
+        "stock" => 8,
+        "delivery" => "20-3-2026",
+        "ingredients" => "• Nicotine=> 200mg ",
+        "image" => "💊"
+    ],
+
+    [
+        "id" => 13,
+        "name" => "De Zaanse Hoeve Halfvolle melk",
+        "category" => "Melk & Yoghurt",
+        "price" => "1.09",
+        "stock" => 5,
+        "delivery" => "17-3-2026",
+        "ingredients" => "• Energie=> 47 kcal<br>• Vet=> 1.5g",
+        "image" => "🥛"
+    ],
+    [
+        "id" => 14,
+        "name" => "Griekse Yoghurt",
+        "category" => "Melk & Yoghurt",
+        "price" => "2.15",
+        "stock" => 12,
+        "delivery" => "17-3-2026",
+        "ingredients" => "• Energie=> 47 kcal<br>• Vet=> 1.5g",
+        "image" => "🥣"
+    ],
+    [
+        "id" => 15,
+        "name" => "AH Rundergehakt 500g",
+        "category" => "Vlees",
+        "price" => "4.85",
+        "stock" => 10,
+        "delivery" => "17-3-2026",
+        "ingredients" => "• 100% Rundvlees",
+        "image" => "🥩"
+    ],
+    [
+        "id" => 16,
+        "name" => "AH Spekreepjes",
+        "category" => "Vlees",
+        "price" => "2.95",
+        "stock" => 14,
+        "delivery" => "18-3-2026",
+        "ingredients" => "• Varkensvlees<br>• Zout",
+        "image" => "🥓"
+    ],
+    [
+        "id" => 17,
+        "name" => "AH Roomijs Vanille",
+        "category" => "Ijs",
+        "price" => "2.49",
+        "stock" => 15,
+        "delivery" => "17-3-2026",
+        "ingredients" => "• Slagroom<br>• Melk<br>• Vanille-aroma",
+        "image" => "🍦"
+    ],
+    [
+        "id" => 18,
+        "name" => "Magnum Almond (3-pack)",
+        "category" => "Ijs",
+        "price" => "4.15",
+        "stock" => 8,
+        "delivery" => "18-3-2026",
+        "ingredients" => "• Melkchocolade<br>• Amandelen<br>• Vanille-ijs",
+        "image" => "🍦"
+    ]
+];
+
+
 
 
 
