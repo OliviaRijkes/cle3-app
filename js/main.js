937f469df8898e982e1a8a1b485d3c1e450ba886
OliviaRijkes <1101595@hr.nl>
2026-03-31 11:17:59 +0200

    location basis and clickhandler

---
+++ js/main.js
@@ -0,0 +1,56 @@
+let lookDirection
+let position
+let goal = {x: 0, y:0}
+let productspace
+
+window.addEventListener('load', init)
+
+function init(){
+    fetchStuff('http://cle3-app.test/webservice/',loadProducts)
+    productspace = document.getElementById('productspace')
+    productspace.addEventListener('click', productClickHandler)
+
+}
+function fetchStuff(url, callback) {
+    fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(callback)
+        .catch(fetchErrorHandler);
+}
+
+function fetchErrorHandler(data) {
+    console.error(data);
+}
+
+
+function productClickHandler(e){
+    if(e.target.nodeName !=='BUTTON'){
+        return;
+    }
+    if (e.target.id === 'details'){
+        fetchStuff(`http://cle3-app.test/webservice/?id=${e.target.dataset.id}`, loadDetails)
+    }
+    if (e.target.id === 'goal'){
+        goal = [e.target.dataset.x,e.target.dataset.y]
+        //turn the cpx locator on
+    }
+}
+function loadProducts(data){
+    //fill the productSpace
+}
+function loadDetails(product){
+    //details pop-up
+}
+
+//if the user hits an intersection
+function showNewDirection(){
+    //first go up||down
+
+
+}
+
