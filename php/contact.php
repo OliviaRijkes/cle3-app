530c1c9d662d70e169844a04a7d8dadad3bf9828
Tuna <tkelemci@student.lentiz.nl>
2026-04-13 11:59:34 +0200

    Testen contactpagina

--- php/contact.php
+++ php/contact.php
@@ -1,13 +1,10 @@
-<?php
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
     <title>Contactpagina</title>
-    <link rel="stylesheet" href="css/contact.css">
+    <link rel="stylesheet" href="../css/contact.css">
 </head>
 <body>
 <nav>
@@ -16,7 +13,7 @@
              alt="Logo van Shoppi">
         <a href="index.html"> Home </a>
         <a href="info.html"> About Shoppi </a>
-        <a href="contact.php">Contactpagina</a>
+        <a href="php/contact.php">Contactpagina</a>
 
     </div>
 </nav>
@@ -29,14 +26,13 @@
             <label for="first-name">Naam:</label>
             <input type="text" id="first-name" name="name" required placeholder="voornaam"/>
 
-            <label for="last-name">achternaam:</label>
+            <label for="last-name">Achternaam:</label>
             <input type="text" id="last-name" name="lastname" required placeholder="achternaam"/>
 
             <label for="email">Email</label>
             <input type="email" id="email" name="email" required placeholder="e-mail"/>
 
-
-            jouw vraag
+            <label for="question"> Jouw vraag </label>
             <textarea name="question" rows="2" cols="30">
 
 </textarea>
